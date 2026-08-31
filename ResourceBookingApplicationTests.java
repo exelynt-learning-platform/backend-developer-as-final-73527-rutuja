961b24ee8f976774cfabd9f4ff3b8b5a08057679
Rsalunkhe03 <114497092+Rsalunkhe03@users.noreply.github.com>
2026-08-31 22:07:08 +0530

    Add files via upload

---
+++ ResourceBookingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rutuja.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceBookingApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
